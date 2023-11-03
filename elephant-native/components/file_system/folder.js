f3d863f7c9346565b59f20517b98af731720e4e3
William Rainey <williamhrainey@gmail.com>
2023-11-03 14:59:01 -0700

    setup menu for managing files and folders

--- elephant-native/components/file_system/folder.js
+++ elephant-native/components/file_system/folder.js
@@ -1,18 +1,36 @@
 import { StyleSheet, Text, View, TouchableOpacity } from 'react-native';
-import React from 'react'
+import React, {useState} from 'react'
 
 import { FontAwesomeIcon } from '@fortawesome/react-native-fontawesome';
-import { faArrowRight, faFolder } from '@fortawesome/free-solid-svg-icons';
+import { faEllipsisVertical, faFolder } from '@fortawesome/free-solid-svg-icons';
 
 const Folder = ({folder, getTargetFolder, pressable}) => {
+
+  const [visible, setVisible] = useState(false)
+
   return (
-    <TouchableOpacity style={styles.folder} onPress={() => getTargetFolder(folder)}>
-        <View style={styles.folderTitle}>
-            <FontAwesomeIcon icon={faFolder} color={'white'} size={32} />
-            <Text style={styles.folderName}>{folder.fileName}</Text>
-        </View>
-        <FontAwesomeIcon icon={faArrowRight} size={26} color={'white'} style={styles.folderArrow}/>
-    </TouchableOpacity>
+    <View>
+      <View style={visible ? styles.folderVisibleMenu : styles.folder}>
+        <TouchableOpacity onPress={() => getTargetFolder(folder)} style={{width: '95%'}}>
+            <View style={styles.folderTitle}>
+                <FontAwesomeIcon icon={faFolder} color={'white'} size={32} />
+                <Text style={styles.folderName}>{folder.fileName}</Text>
+            </View>
+        </TouchableOpacity>
+        <TouchableOpacity onPress={() => setVisible(prev => !prev)}>
+          <FontAwesomeIcon icon={faEllipsisVertical} size={26} color={'white'} style={styles.folderArrow}/>
+        </TouchableOpacity>
+    </View>
+    {visible ?
+      <View style={{width: '90%', height: 100, marginLeft: 'auto', marginRight: 'auto', borderWidth: 2, borderColor: 'red', display: 'flex', flexDirection: 'row', justifyContent: 'flex-end', position: 'fixed'}}>
+          <View style={{position: 'absolute', width: '50%', height: '100%', backgroundColor: 'white'}}>
+            <Text>Rename Folder</Text>
+            <Text>Delete Folder</Text>
+            <Text>Move Folder To</Text>
+          </View>
+      </View>
+      : <></>}
+    </View>
   )
 }
 
@@ -35,6 +53,19 @@ const styles = StyleSheet.create({
         paddingLeft: '4%',
         marginBottom: '8%'
     },
+    folderVisibleMenu: {
+      display: 'flex',
+      justifyContent: 'space-between',
+      paddingRight: '2%',
+      flexDirection: 'row',
+      marginLeft: 'auto',
+      marginRight: 'auto',
+      borderBottomWidth: '2px',
+      borderBottomColor: 'white',
+      width: '90%',
+      paddingBottom: '1.5%',
+      paddingLeft: '4%',
+  },
     folderTitle: {
       display: 'flex',
       flexDirection: 'row',
@@ -46,7 +77,7 @@ const styles = StyleSheet.create({
       fontSize: 22,
       fontWeight: '500',
       paddingTop: '3%',
-      marginLeft: '6%'
+      marginLeft: '5%'
     },
     folderArrow: {
     marginTop: 'auto'
