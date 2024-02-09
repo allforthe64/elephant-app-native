e8ce5ae1721e3b09a6f30f358b4f8873561e5532
allforthe64 <williamhrainey@gmail.com>
2024-02-09 11:07:47 -0800

    reset to default

--- elephant-native/screens/dashboardStackScreens/docScanner.js
+++ elephant-native/screens/dashboardStackScreens/docScanner.js
@@ -3,7 +3,7 @@ import { Image } from 'react-native'
 import DocumentScanner from 'react-native-document-scanner-plugin'
 
 const DocScanner = () => {
-  const [scannedImageArray, setScannedImageArray] = useState();
+  const [scannedImage, setScannedImage] = useState();
 
   const scanDocument = async () => {
     // start the document scanner
@@ -12,7 +12,7 @@ const DocScanner = () => {
     // get back an array with scanned image file paths
     if (scannedImages.length > 0) {
       // set the img src, so we can view the first scanned image
-      setScannedImageArray(scannedImages)
+      setScannedImage(scannedImages[0])
     }
   }
 
@@ -22,17 +22,11 @@ const DocScanner = () => {
   }, []);
 
   return (
-    <>
-      {scannedImageArray ? 
-        <Image
-          resizeMode="contain"
-          style={{ width: '100%', height: '100%' }}
-          source={{ uri: scannedImageArray[0] }}
-        />
-      :
-        <></>
-      }
-    </>
+    <Image
+      resizeMode="contain"
+      style={{ width: '100%', height: '100%' }}
+      source={{ uri: scannedImage }}
+    />
   )
 }
 
