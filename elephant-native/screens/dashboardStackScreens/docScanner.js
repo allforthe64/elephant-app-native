d404e12a1da9a3a1db826fd263df7ec5b5bb4cb4
allforthe64 <williamhrainey@gmail.com>
2024-02-10 16:53:29 -0800

    tried to add the carousel back in and request permissions for android

--- elephant-native/screens/dashboardStackScreens/docScanner.js
+++ elephant-native/screens/dashboardStackScreens/docScanner.js
@@ -1,18 +1,34 @@
 import React, { useState, useEffect } from 'react'
-import { Image } from 'react-native'
+import { Image, Platform, PermissionsAndroid, Dimensions } from 'react-native'
 import DocumentScanner from 'react-native-document-scanner-plugin'
+import { useToast } from 'react-native-toast-notifications'
 
 const DocScanner = () => {
-  const [scannedImage, setScannedImage] = useState();
+  const [scannedImageArray, setScannedImageArray] = useState();
+
+  const toast = useToast()
+
+  const width = Dimensions.get('window').width
 
   const scanDocument = async () => {
+
+    // prompt user to accept camera permission request if they haven't already
+    if (Platform.OS === 'android' && await PermissionsAndroid.request(
+      PermissionsAndroid.PERMISSIONS.CAMERA
+    ) !== PermissionsAndroid.RESULTS.GRANTED) {
+      toast.show('File upload successful', {
+        type: 'error'
+      }) 
+      return
+    }
+
     // start the document scanner
     const { scannedImages } = await DocumentScanner.scanDocument()
   
     // get back an array with scanned image file paths
     if (scannedImages.length > 0) {
       // set the img src, so we can view the first scanned image
-      setScannedImage(scannedImages[0])
+      setScannedImageArray(scannedImages)
     }
   }
 
@@ -22,11 +38,36 @@ const DocScanner = () => {
   }, []);
 
   return (
-    <Image
-      resizeMode="contain"
-      style={{ width: '100%', height: '100%' }}
-      source={{ uri: scannedImage }}
-    />
+    <>
+      {scannedImageArray ? 
+        <View style={{ flex: 1 }}>
+            <Carousel
+                loop
+                width={width}
+                height={width / 2}
+                autoPlay={true}
+                data={scannedImageArray}
+                scrollAnimationDuration={1000}
+                onSnapToItem={(index) => console.log('current index:', index)}
+                renderItem={({ index }) => (
+                    <View
+                        style={{
+                            flex: 1,
+                            borderWidth: 1,
+                            justifyContent: 'center',
+                        }}
+                    >
+                        <Image 
+                          source={{uri: scannedImageArray[index]}}
+                        />
+                    </View>
+                )}
+            />
+        </View>
+      :
+        <></>
+      }
+    </>
   )
 }
 
