4845f3f1de9472a841659ff4ee6d839916db0e29
allforthe64 <williamhrainey@gmail.com>
2024-02-20 10:25:36 -0800

    same ish

--- elephant-native/screens/dashboardStackScreens/dashMain.js
+++ elephant-native/screens/dashboardStackScreens/dashMain.js
@@ -6,6 +6,7 @@ import { FontAwesomeIcon } from '@fortawesome/react-native-fontawesome';
 import { faFolder, faCamera, faBox } from '@fortawesome/free-solid-svg-icons';
 import { useSafeAreaInsets } from 'react-native-safe-area-context';
 import { useToast } from 'react-native-toast-notifications';
+import RNBlobUtil from 'react-native-blob-util';
 
 export default function DashMain({navigation: { navigate }}) {
   const auth = firebaseAuth
@@ -19,6 +20,8 @@ export default function DashMain({navigation: { navigate }}) {
   }, [])
 
   const insets = useSafeAreaInsets()
+
+  console.log(RNBlobUtil.fs.dirs.DocumentDir)
 
   return (
     <View>
