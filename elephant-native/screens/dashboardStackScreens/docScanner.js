982ae9f4331cffbc79eb9e64c20b231296d728b9
allforthe64 <williamhrainey@gmail.com>
2024-03-29 09:53:32 -0700

    making sure to give the ref the same name as the storage object

--- elephant-native/screens/dashboardStackScreens/docScanner.js
+++ elephant-native/screens/dashboardStackScreens/docScanner.js
@@ -165,7 +165,7 @@ const DocScanner = () => {
           xhr.send(null)
       })
       
-      const filename = docName !== '' ? `${currentUser}/${docName}` : `${currentUser}/${formattedDate}`
+      const filename = docName !== '' ? `${currentUser}/${docName}.pdf` : `${currentUser}/${formattedDate}.pdf`
       const fileRef = ref(storage, filename)
       const result = await uploadBytes(fileRef, blob)
 
@@ -178,7 +178,7 @@ const DocScanner = () => {
       
       //generate references
       const reference = await addfile({
-        name: `${formattedDate}.pdf`,
+        name: fileName,
         fileType: 'pdf',
         size: result.metadata.size,
         uri: path,
